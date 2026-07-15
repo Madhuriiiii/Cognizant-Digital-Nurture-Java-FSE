83ce22b1c3f09b7fabd5b2bd9178559a4ce90f39
Madhuriiiii <madhuripjr@gmail.com>
2026-07-15 23:22:21 +0530

    Delete SpringDataJPAExample directory

--- SpringDataJPAExample/src/main/java/com/cognizant/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cognizant.repository;
-
-import com.cognizant.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
